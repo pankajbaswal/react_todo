e280dedf684ea2f3bdbd2d26c3da08770f63b2e9
Pankaj Baswal <pankajbaswal3360@gmail.com>
2025-08-03 10:25:08 +0530

    added editting and finished todo logic

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 
-function Todo( { deleteTodo , text , isFinished } ){
-
+function Todo( { editTodo , deleteTodo , text , isFinished } ){
+    const [isEditting , setIsEditting] = useState(false);
+    const [todoText , setTodoText]=useState(text);
     return(
         <div>
             <input type="checkbox" checked ={isFinished} />
-            <span>{text}</span>
-            <button>Edit</button>
+            {isEditting ? <input value={todoText} onChange={(e) => setTodoText(e.target.value)}/> : <span>{text}</span>}
+            <button onClick={() => {
+                setIsEditting(!isEditting);
+                editTodo(todoText);
+            }}>{isEditting? "Save":"Edit"}</button>
             <button onClick={deleteTodo}>Delete</button>
         </div>
     );
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,26 +1,45 @@
-import Todo from "../Todo/Todo"
-function TodoList({ todos ,setTodos}) {
+import Todo from "../Todo/Todo";
+function TodoList({ todos, setTodos }) {
+  function onDeleteTodo(id) {
+    const newTodo = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodo);
+  }
 
-    function onDeleteTodo(id){
-        console.log(todos)
-        const newTodo= todos.filter(todo => todo.id !== id);
-        console.log(newTodo);
-        setTodos(newTodo);
-       
-    }
+  function onEditTodo(id, newTodo) {
+    const newTodoList = todos.map((todo) => {
+      if (todo.id == id) {
+        todo.text = newTodo;
+      }
+      return todo;
+    });
+    setTodos(newTodoList);
+}
+   function onFinished(id , state){
+    const newTodoList=todos.map(()=>{
+        if(todo.id==id){
+            todo.isFinished=state;
+        }
+        return todo;
+    });
+    setTodos(newTodoList);
 
-    return (
-        <div>
-            {todos && todos.map((todo) => (
-                <Todo 
-                    key={todo.id}
-                    text={todo.text}
-                    isFinished={todo.isFinished}
-                    deleteTodo={()=> onDeleteTodo(todo.id)}
-                />
-            ))}
-        </div>
-    );
+
+} 
+  return (
+    <div>
+      {todos &&
+        todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            text={todo.text}
+            isFinished={todo.isFinished}
+            editTodo={(newTodo) => onEditTodo(todo.id, newTodo)}
+            deleteTodo={() => onDeleteTodo(todo.id)}
+            finishedTodo ={(state) =>onFinished(todo.id , state)}
+          />
+        ))}
+    </div>
+  );
 }
 
 export default TodoList;
